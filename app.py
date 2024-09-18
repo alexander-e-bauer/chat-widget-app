228ff97b228f7c17bcd8a2f79cfee1c955d8292a
alexander-e-bauer <71612093+alexander-e-bauer@users.noreply.github.com>
2024-09-18 14:58:49 -0500

    Update Procfile for new structure

--- app.py
+++ app.py
@@ -1,8 +1,48 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-
+import config
 app = Flask(__name__)
 CORS(app)
+
+def chat_completion(user_input, system_input="you are a helpful assistant", image_path=None, tools=None,
+                    streaming=False):
+    if image_path is None:
+        messages = [
+            {"role": "system", "content": system_input},
+            {"role": "user", "content": user_input}
+        ]
+        completion = openai_client.chat.completions.create(
+            model="gpt-4o",
+            messages=messages,
+            stream=streaming
+        )
+    else:
+        messages = [
+            {"role": "system", "content": system_input},
+            {"role": "user", "content": [
+                {"type": "image", "data": image_path},
+                {"type": "text", "text": user_input}
+            ]}
+        ]
+        completion = openai_client.chat.completions.create(
+            model="gpt-4o",
+            messages=messages,
+            tools=tools,
+            tool_choice="auto",
+            stream=streaming
+        )
+    if not streaming:
+        output = completion.choices[0].message.content
+        print(output)
+        return output
+    else:
+        output = ""
+        for chunk in completion:
+            output += str(chunk.choices[0].delta.content)
+            print(chunk.choices[0].delta.content)
+        print(output)
+        return output
+
 
 @app.route('/api/chat', methods=['POST'])
 def chat():
