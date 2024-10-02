608e92f718fa5ba2a42e85180ffec9eebe09cd10
alexander-e-bauer <71612093+alexander-e-bauer@users.noreply.github.com>
2024-10-02 14:45:09 -0500

    “cw”

--- app.py
+++ app.py
@@ -4,30 +4,6 @@ from flask_socketio import SocketIO, emit
 import config
 import logging
 
-
-import os
-import dotenv
-import datetime
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin
-from flask_security import RegisterForm, LoginForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
-from flask_bootstrap import Bootstrap
-from flask_wtf.csrf import CSRFProtect
-from xyz.llm import llm_blueprint
-from xyz.database import database, models
-
-
-class ExtendedRegisterForm(RegisterForm):
-    first_name = StringField('First Name', [DataRequired()])
-    last_name = StringField('Last Name', [DataRequired()])
-
-class ExtendedLoginForm(LoginForm):
-    email = StringField('Email Address', [DataRequired()])
-
-
 # Set up logging
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -35,53 +11,11 @@ logger = logging.getLogger(__name__)
 openai_client = config.openai_client
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
-
-username_db = os.getenv('POSTGRES_USER')
-password_db = os.getenv('POSTGRES_PASSWORD')
-database_name = os.getenv('POSTGRES_DB')
-postgres_uri = f'postgresql://{username_db}:{password_db}@localhost/{database_name}'
-app.config['SQLALCHEMY_DATABASE_URI'] = postgres_uri
-
-app.config['SECURITY_PASSWORD_SALT'] = os.environ.get("SECURITY_PASSWORD_SALT")
-
-
-# Create database connection object
-db = database.init_app(app)
-
-# Define models
-User, Role, roles_users = models.define_models(db)
-
-
-# Setup Flask-Security
-user_datastore = SQLAlchemyUserDatastore(db, User, Role)
-app.config['SECURITY_REGISTERABLE'] = True
-app.config['SECURITY_RECOVERABLE'] = True
-app.config['SECURITY_CHANGEABLE'] = True
-app.config['SECURITY_REGISTER_URL'] = '/register'
-app.config['SECURITY_LOGIN_URL'] = '/login'
-app.config['SECURITY_LOGOUT_URL'] = '/logout'
-app.config['SECURITY_RESET_URL'] = '/reset'
-app.config['SECURITY_CHANGE_URL'] = '/change'
-
-security = Security(app, user_datastore,
-                    register_form=ExtendedRegisterForm,
-                    login_form=ExtendedLoginForm)
-
-# CSRF
-csrf = CSRFProtect(app)
-
-# Bootstrap
-Bootstrap(app)
-
-# Blueprints
-oai = llm_blueprint.init_app(app)
-
 # Update CORS configuration
 CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "https://alexander-e-bauer.github.io"]}})
 
 # Update SocketIO configuration
-socketio = SocketIO(app, cors_allowed_origins=["http://localhost:3000", "https://alexander-e-bauer.github.io"])
+socketio = SocketIO(app, cors_allowed_origins=["http://localhost:3000", "https://alexander-e-bauer.github.io"], async_mode='gevent')
 
 # In-memory storage for conversation history
 conversation_history = {}
